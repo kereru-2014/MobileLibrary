0419a1d0695003bf767f8bafbe9c483c3c72f966
Daniel Lewis & Grace Fox <pair+chronicd.gracefox@devbootcamp.com>
2014-09-25 21:50:47 +1200

    added apipie

---
+++ mobile_library/README.md
@@ -0,0 +1,6 @@
+API Documentation
+=================
+
+We use APIPIE to serve our API documentation to users.
+
+go to [/apipie](/apipie)
--- mobile_library/app/controllers/books_controller.rb
+++ mobile_library/app/controllers/books_controller.rb
@@ -1,6 +1,22 @@
 class BooksController < ApplicationController
   protect_from_forgery with: :null_session
 
+# v1/books_controller.rb
+  def_param_group :book do
+    param :book, Hash, :required => false, :action_aware => true do
+      param :title, String, "Book title"
+      param :author, String, "Authors name"
+      param :ISBN, String, "ISBN number"
+      param :owner_id, Integer, "User Id of book owner"
+      param :borrower_id, Integer, "User Id of book borrower"
+      param :lent_date, String, "Date lent out - as string at mo"
+      param :reminder_date, String, "Date book is due - Note is string at mo"
+      param :image, String, "Image name - as string at mo"
+    end
+  end
+
+  api :GET, '/v1'
+  param_group :book
   def index
     render json: Book.all
   end
@@ -9,6 +25,9 @@ class BooksController < ApplicationController
     render json: Book.create!(book_params)
   end
 
+  api :GET, '/v1/books/:id'
+  param :id, Integer, "Id of book"
+  param_group :book
   def show
     render json: Book.find(params[:id])
   end
