0666924a405c5a84d32628c9e5fd7d2c7a0339ff
Daniel Lewis & Grace Fox <pair+chronicd.gracefox@devbootcamp.com>
2014-09-27 18:01:13 +1200

    changed tables for lend function to be in correct table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927002636) do
+ActiveRecord::Schema.define(version: 20140927055539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 20140927002636) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_url"
+    t.integer  "borrower_id"
   end
 
   create_table "borrowers", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.integer  "phone_number"
-    t.integer  "book_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
